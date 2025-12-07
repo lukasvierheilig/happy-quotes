7e77f9574f2b8b5bb31114fc768725d700064dec
Lukas Vierheilig <lukas.vierheilig@gmail.com>
2025-12-07 14:22:31 +0100

    Initialize project

---
+++ src/main/java/dev/lukas/happyquotes/HappyQuotesApplication.java
@@ -0,0 +1,13 @@
+package dev.lukas.happyquotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyQuotesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyQuotesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/lukas/happyquotes/HappyQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lukas.happyquotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HappyQuotesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
