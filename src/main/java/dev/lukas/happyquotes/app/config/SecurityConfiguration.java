676093bccea04d880a1fc1564444679d4037e14e
Lukas Vierheilig <lukas.vierheilig@gmail.com>
2025-12-10 15:19:44 +0100

    Allow unauthenticated GET requests to `api/quotes/**`

--- src/main/java/dev/lukas/happyquotes/app/config/SecurityConfiguration.java
+++ src/main/java/dev/lukas/happyquotes/app/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package dev.lukas.happyquotes.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -17,8 +18,10 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeHttpRequests((authorize) -> authorize
-                        .anyRequest().authenticated()
+                .authorizeHttpRequests((authorize) -> {
+                            authorize.requestMatchers(HttpMethod.GET, "api/quotes/**").permitAll();
+                            authorize.anyRequest().authenticated();
+                        }
                 )
                 .cors(withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
